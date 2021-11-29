2d4c0eafab79ebf3b6b1461a44758d200a3881fa
sonmansu <gjdmstnals@pusan.ac.kr>
2021-11-30 04:38:26 +0900

    Add toy(servo motor) code and slider html examples

--- sm/rc_car/WEMOSD1_RC_CAR_Ultrasonic/WEMOSD1_RC_CAR_Ultrasonic.ino
+++ sm/rc_car/WEMOSD1_RC_CAR_Ultrasonic/WEMOSD1_RC_CAR_Ultrasonic.ino
@@ -1,11 +1,13 @@
 // Simple WiFi-controlled car on ESP8266 and L9110S motor drive board by Alan Wang
 // https://www.hackster.io/alankrantas/simple-nodemcu-wifi-controlled-car-esp8266-c5491e
-/* 위 주소의 코드를 우리 상황에 맞게 수정함
+/* 위 주소의 코드(웹에서 차 주행)를 우리 상황에 맞게 수정함
     1. AP mode 를 station mode로 변경
     2. 핀 매핑 및 차 운전 방식은 키트 판매 싸이트 코드 따라함
     3. html 파일 분리
-    4. 초음파센서 연결 : https://m.blog.naver.com/no1_devicemart/221312209045
+    4. 초음파센서 연결, 자율주행 기능 : https://m.blog.naver.com/no1_devicemart/221312209045
+    5. 서보모터 - 장난감 
 */
+
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include "html.h"
@@ -94,6 +96,8 @@ void setup() {
   server.on("/right", HTTP_GET, handle_right);
   server.on("/stop", HTTP_GET, handle_stop);
   server.on("/obstacle", HTTP_GET, handle_obstacle);
+  server.on("/toy", HTTP_GET, handle_toy);
+
 
   server.onNotFound(handle_NotFound);
 
--- sm/servo_motor/servo_motor04_toy/servo_motor04_toy.ino
+++ sm/servo_motor/servo_motor04_toy/servo_motor04_toy.ino
@@ -1,7 +1,6 @@
-//https://www.youtube.com/watch?v=ZsvNVPCetmI&ab_channel=H_World 보고 서보모터 소음잡는 코드로 수정
+//최종사용코드
 #include <Servo.h>
 Servo myservo; // create servo object to control a servo
-int pos = 0; // variable to store the servo position
 
 int servoAngle = 0;
 void setup()
@@ -9,17 +8,66 @@ void setup()
   // attaches the servo on pin 10 to the servo object
   Serial.begin(9600);
   myservo.attach(2);
+  Serial.println("hi");
 }
-
 void loop()
 {
-//위에거랑 소음이 비슷한거같아서 그냥 이거 쓰기로..
+  //위에거랑 소음이 비슷한거같아서 그냥 이거 쓰기로..
   if (Serial.available()) {
-    if (Serial.read() == 'd') {
-      Serial.println('d');
-      myservo.write(130); //문열음
-      delay(3000); //3초 기다림
-      myservo.write(0);   //문닫음
-    }
+    char c = Serial.read();
+    Serial.println(c);
+    if (c == '3') runServo(300);
+    else if (c == '4') runServo(400);
+    else if (c == '5') runServo(500);
+    else if (c == '6') runServo(600);
+    else if (c == '7') runServo(700);
+    else if (c == '8') runServo(800);
+
+    else if (c == 'e') runServo1(300);
+    else if (c == 'r') runServo1(400);
+
+    else if (c == 'p') runRandomAngle(); //쓰레기임 
+    else if (c == 'l') runRandomDelay();
+
+  }
+}
+void runServo(int speed) { //delay는 300이상 이어야..
+  while(true) {
+    if (Serial.available()) break; //end
+    myservo.write(180); //최대각도
+    delay(speed);
+    myservo.write(0); //최소각도
+    delay(speed);
+  }
+}
+void runServo1(int speed) { //소음 감소 코든데 똑같은듯 걍 위에꺼 쓰자..
+  while(true) {
+    myservo.attach(2);
+    if (Serial.available()) break; //end
+    myservo.write(180); //최대각도
+    delay(speed);
+    myservo.write(0); //최소각도
+    delay(speed);
+    myservo.detach();
+  }
+}
+void runRandomAngle() { //영 노잼 
+  while(true) {
+    if (Serial.available()) break; 
+    int angle = random(0, 90);
+    myservo.write(angle);
+    delay(300);
+  }
+}
+void runRandomDelay() { // 
+  int delay_time;
+  while(true) {
+    if (Serial.available()) break; 
+    delay_time = random(300, 900);
+    myservo.write(0);
+    delay(delay_time);
+    myservo.write(180);
+    delay_time = random(300, 900);
+    delay(delay_time);
   }
 }
