f844d772a469f771ea06188a8e3be11f5832001a
sonmansu <gjdmstnals@pusan.ac.kr>
2021-12-01 05:36:50 +0900

    Complement toy(servo motor) code
    File name: myslider03

--- sm/slider/myslider02/index_html.h
+++ sm/slider/myslider02/index_html.h
@@ -8,16 +8,16 @@ const char index_html[] PROGMEM = R"rawliteral(
     h2 {font-size: 2.3rem;}
     p {font-size: 1.9rem;}
     body {max-width: 400px; margin:0px auto; padding-bottom: 25px;}
-    .slider { -webkit-appearance: none; margin: 14px; width: 360px; height: 25px; background: #FFD65C;
-      outline: none; -webkit-transition: .2s; transition: opacity .2s;}
-    .slider::-webkit-slider-thumb {-webkit-appearance: none; appearance: none; width: 35px; height: 35px; background: #003249; cursor: pointer;}
-    .slider::-moz-range-thumb { width: 35px; height: 35px; background: #003249; cursor: pointer; }
+    /*.slider { -webkit-appearance: none; margin: 14px; width: 360px; height: 25px; background: #FFD65C;*/
+    /*  outline: none; -webkit-transition: .2s; transition: opacity .2s;}*/
+    /*.slider::-webkit-slider-thumb {-webkit-appearance: none; appearance: none; width: 35px; height: 35px; background: #003249; cursor: pointer;}*/
+    /*.slider::-moz-range-thumb { width: 35px; height: 35px; background: #003249; cursor: pointer; }*/
   </style>
 </head>
 <body>
   <h2>ESP Web Server</h2>
-  <p><span id="textSliderValue">%SLIDERVALUE%</span></p>
-  <p><input type="range" onchange="updateSliderPWM(this)" id="pwmSlider" min="0" max="1023" value="%SLIDERVALUE%" step="1" class="slider"></p>
+  <p><span id="textSliderValue">400</span></p>
+  <p><input type="range" onchange="updateSliderPWM(this)" id="pwmSlider" min="300" max="900" step="100" value="400" step="1" class="slider"></p>
 <script>
 function updateSliderPWM(element) {
   var sliderValue = document.getElementById("pwmSlider").value;
--- sm/slider/myslider03_cleanup02/myslider03_cleanup02.ino
+++ sm/slider/myslider03_cleanup02/myslider03_cleanup02.ino
@@ -1,15 +1,5 @@
-//https://randomnerdtutorials.com/esp8266-nodemcu-web-server-slider-pwm/
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-web-server-slider-pwm/
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
-//별 시행착오 포함된 코드
+//https://randomnerdtutorials.com/esp8266-nodemcu-web-server-slider-pwm/ 에 수정함.
+//myslider02를 정리한 코드
 
 // Import required libraries
 #include <ESP8266WiFi.h>
@@ -24,8 +14,8 @@ const char* password = "sumin78900";
 
 const int output = 2;
 
-String sliderValue = "0";
-boolean flagServo = false;
+int sliderValue = 400; //초깃값
+boolean flagToy = false;
 
 const char* PARAM_INPUT = "value";
 
@@ -52,20 +42,29 @@ void setup() {
   server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send_P(200, "text/html", index_html);
   });
-
+  server.on("/servoButton", HTTP_GET, [](AsyncWebServerRequest * request) {
+    String inputMessage;
+    // GET input1 value on <ESP_IP>/slider?value=<inputMessage>
+    if (request->hasParam(PARAM_INPUT)) {
+      inputMessage = request->getParam(PARAM_INPUT)->value();
+      flagToy = inputMessage == "1";
+      Serial.println((String) "flagToy: " + flagToy);
+    }
+    else {
+      inputMessage = "No message sent";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/plain", "OK");
+  });
   // Send a GET request to <ESP_IP>/slider?value=<inputMessage>
   server.on("/slider", HTTP_GET, [] (AsyncWebServerRequest * request) {
     String inputMessage;
     // GET input1 value on <ESP_IP>/slider?value=<inputMessage>
     if (request->hasParam(PARAM_INPUT)) {
       inputMessage = request->getParam(PARAM_INPUT)->value();
-      sliderValue = inputMessage;
-      //      int intSliderVal = sliderValue.toInt();
-      //      if (intSliderVal == 900) runRandomDelay();
-      //      else runServo(intSliderVal);
-      analogWrite(output, sliderValue.toInt());
-//      runServo(sliderValue.toInt());
-      flagServo = true;
+      sliderValue = inputMessage.toInt();
+//      analogWrite(output, sliderValue.toInt()); //led키는거 
+//      runServo(sliderValue.toInt()); //여기선 동작안함
     }
     else {
       inputMessage = "No message sent";
@@ -79,49 +78,26 @@ void setup() {
 }
 
 void loop() {
-//  int intSliderVal = sliderValue.toInt();
-//  if (intSliderVal == 900) runRandomDelay();
-//  else runServo(intSliderVal);
-  if (flagServo) runServo(sliderValue.toInt());
+  if (flagToy) {
+    if (sliderValue == 900) runRandomDelay();
+    else runServo(sliderValue);
+  }
 }
-//void myDelay(int delay_time) {
-//  for (int i = 0; i < delay_time; i++) {}
-//}
-//void runServo(int delay_time) { //delay는 300이상 이어야..
-////  while (true) {
-//    myservo.write(180); //최대각도
-//    myDelay(delay_time);
-//    myservo.write(0); //최소각도
-//    myDelay(delay_time);
-////  }
-//}
-
-//void runServo(int delay_time) { //delay는 300이상 이어야..
-//    unsigned long time_now = 0;
-//    int angle = 0;
-//
-//  while  (true) {
-//    if (millis() > time_now + delay_time) {
-//      angle = (angle == 0) ? 180 : 0; //0이면 180사용, 0이 아니면 0 사용 
-//      myservo.write(angle); //최소각도
-//      time_now = millis();
-//    }
-//  }
-//}
 
 void runServo(int delay_time) { //delay는 300이상 이어야..
-  while (true) {
+  Serial.println((String) "delay_time: "+ delay_time);
+//  while (true) {
 //    if (Serial.available()) break; //end
     myservo.write(180); //최대각도
     delay(delay_time);
       myservo.write(0); //최소각도
     delay(delay_time);
-  }
+//  }
 }
 
 void runRandomDelay() { //
   int delay_time;
-  while (true) {
+//  while (true) {
 //    if (Serial.available()) break;
     delay_time = random(300, 900);
     myservo.write(0);
@@ -129,5 +105,5 @@ void runRandomDelay() { //
     myservo.write(180);
     delay_time = random(300, 900);
     delay(delay_time);
-  }
+//  }
 }
