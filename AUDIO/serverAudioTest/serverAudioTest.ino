2d47cca6369c87d31839165db4b51beaaa00dc19
Jinhyeok Jeon <aqwstn@naver.com>
2021-12-05 17:25:24 +0900

    New commit

--- AUDIO/serverAudioTest/serverAudioTest.ino
+++ AUDIO/serverAudioTest/serverAudioTest.ino
@@ -1,48 +1,136 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServerSecure.h>
 #include "web.h"
 
-#define WIFI_SSID            "KT_GiGA_2G_sumin" // AP server name
-#define WIFI_PASSWORD        "sumin78900"         // AP server password
+//#define WIFI_SSID            "KT_GiGA_2G_sumin" // AP server name
+//#define WIFI_PASSWORD        "sumin78900"         // AP server password
 
-
-IPAddress local_ip(172, 30, 1, 130); // 사용할 IP 주소
-IPAddress gateway(172, 30, 1, 254); // 게이트웨이 주소
-IPAddress subnet(255, 255, 255, 0); // 서브넷 주소
-ESP8266WebServer server(80);
-
+const char *ssid = "JJH";
+const char* password = "Jeonjinhyeok0204";
+const char *dname = "esp8266";
 
 String sendHtml;
 
-void setup() {
-  delay(10);
-  Serial.begin(115200);
-  
-  WiFi.config(local_ip, gateway, subnet);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
-  while (WiFi.status() != WL_CONNECTED) {
+BearSSL::ESP8266WebServerSecure server(443);
+ESP8266WebServer serverHTTP(80);
+
+bool connectToWifi() {
+  byte timeout = 50;
+
+  Serial.println("\n\n");
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  for (int i = 0; i < timeout; i++) {
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("\nConnected to WiFi");
+      Serial.print("Server can be accessed at https://");
+      Serial.print(WiFi.localIP());
+      if (MDNS.begin(dname)) {
+        // https://superuser.com/questions/491747/how-can-i-resolve-local-addresses-in-windows
+        Serial.print(" or at https://");
+        Serial.print(dname);
+        Serial.println(".local");
+      }
+      return true;
+    }
+    delay(5000);
     Serial.print(".");
-    delay(500);
   }
-  Serial.println();
-  Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
 
-    server.on("/", handle_OnConnect);
-
-
-  server.begin();
-  Serial.println("ESP8266 server started.");
-  sendHtml = MAIN_page;
+  Serial.println("\nFailed to connect to WiFi");
+  Serial.println("Check network status and access data");
+  Serial.println("Push RST to try again");
+  return false;
 }
+
+void secureRedirect() {
+  serverHTTP.sendHeader("Location", String("https://esp8266.local"), true);
+  serverHTTP.send(301, "text/plain", "");
+}
+
 void handle_OnConnect() {
-//  car_mode = 0;
   Serial.println("Client connected");
   server.send(200, "text/html", sendHtml);
 }
 
+
+static const char serverCert[] PROGMEM = R"EOF(
+//-----BEGIN CERTIFICATE-----
+MIIC6jCCAlOgAwIBAgIUEyHrIFmH1Ug3LQI44Le0v+zuzBMwDQYJKoZIhvcNAQEL
+BQAwejELMAkGA1UEBhMCUk8xCjAIBgNVBAgMAUIxEjAQBgNVBAcMCUJ1Y2hhcmVz
+dDEbMBkGA1UECgwST25lVHJhbnNpc3RvciBbUk9dMRYwFAYDVQQLDA1PbmVUcmFu
+c2lzdG9yMRYwFAYDVQQDDA1lc3A4MjY2LmxvY2FsMB4XDTIxMTEyNDA4NTIwNVoX
+DTIyMTEyNDA4NTIwNVowejELMAkGA1UEBhMCUk8xCjAIBgNVBAgMAUIxEjAQBgNV
+BAcMCUJ1Y2hhcmVzdDEbMBkGA1UECgwST25lVHJhbnNpc3RvciBbUk9dMRYwFAYD
+VQQLDA1PbmVUcmFuc2lzdG9yMRYwFAYDVQQDDA1lc3A4MjY2LmxvY2FsMIGfMA0G
+CSqGSIb3DQEBAQUAA4GNADCBiQKBgQDYqh3WqEShHmKcIQsbIRDXpizsVDBzbzgc
+Rjdcwbv4qPeiwLZH3hWDzuc6Fb7LMkhH1eyLXbCfXy3NvadHoRb6N6DZcNHU07YH
+9vUaMZL5nO2hKdYIsr1ph+190N1WGd6mT/org76ySMUYe3oa904Lf4kuMNmAwm2w
+WhFUuyuFbQIDAQABo20wazAdBgNVHQ4EFgQU8e33IYFa5P5yVkOm8Ilzl/JFTTsw
+HwYDVR0jBBgwFoAU8e33IYFa5P5yVkOm8Ilzl/JFTTswDwYDVR0TAQH/BAUwAwEB
+/zAYBgNVHREEETAPgg1lc3A4MjY2LmxvY2FsMA0GCSqGSIb3DQEBCwUAA4GBACLB
+VsPBAEHjlGTKuTkALWipMiGF0Hiq34VKrzz4I2hh500ypEQvk42ztsQPNiyKewCj
+hBzNydOA8DKmTkLobDio28IfLbBZsoWHdSuhsSZKlBQ31tGmJnBpzOVwRVC+z1Ra
+jFGjq5aurqsodAlVzjaypKWiNPkCGs31j+8+DHOO
+//-----END CERTIFICATE-----
+)EOF";
+
+static const char serverKey[] PROGMEM =  R"EOF(
+//-----BEGIN PRIVATE KEY-----
+MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBANiqHdaoRKEeYpwh
+CxshENemLOxUMHNvOBxGN1zBu/io96LAtkfeFYPO5zoVvssySEfV7ItdsJ9fLc29
+p0ehFvo3oNlw0dTTtgf29Roxkvmc7aEp1giyvWmH7X3Q3VYZ3qZP+iuDvrJIxRh7
+ehr3Tgt/iS4w2YDCbbBaEVS7K4VtAgMBAAECgYBTgl412m15XGjNnL7XGguP0vEF
+tf1mwLtzymyUR+fXk+i+xmB7mtHYF+tvq+n6D8tkRMQDg30MMHdLEYpkRWbBHRB8
+ORswpd56I27/3GpS9h9ri0zyxkNiGpjtF9NGGJfUu4Ze3m7mfo3Zopf7Ob1OXh0p
+0EinssF/DHl/S4Cs0QJBAPCOv9ljPY45PPKyEQ0wTArHXRG44WKnnnRVuYeciUnW
+touV6ztyZiZ60NnDrJeubkdjxOJBlNK/BV9XKhCrAYMCQQDmkrcbjXaglgxivs59
+LtXxTzvBmZXeeYOWVsltSb1UC4f0cE6qV63l2ApqCXnUFb3wj2Ek5RtfRbx41i5Y
+SlpPAkEAveHAGAVY6nHN5TifcwyjiYKSuVF5PxlWXzfnid8LE2jTkN0W4EbN0SPy
+xWHKU7SRQfLDz2C665tyzy4LezqwpwJABJ2rTMHoPg7C8LchKyBk6A/OAnIwreOA
+3uGd5rIzOBuC/UHCUWav9In+q2DFL5m/07ELINH1WlLrEwA2rU3PwQJBAK5OPmdW
+04jmQXUZ70rmIX7DdKXoS9x332o8dyvcvuF7Fx/nsrwfnZQ9rkgNNK/0YmByoSId
+WQxSHZUcCB7CfrA=
+//-----END PRIVATE KEY-----
+)EOF";
+
+//IPAddress local_ip(172, 30, 1, 130); // 사용할 IP 주소
+//IPAddress gateway(172, 30, 1, 254); // 게이트웨이 주소
+//IPAddress subnet(255, 255, 255, 0); // 서브넷 주소
+
+
+
+void setup() {
+  Serial.begin(115200);
+
+  sendHtml = MAIN_page;
+
+  if(!connectToWifi()){
+    delay(60000);
+    ESP.restart();
+  }
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  
+  serverHTTP.on("/", secureRedirect);
+  serverHTTP.begin();
+  
+
+  server.getServer().setRSACert(new BearSSL::X509List(serverCert), new BearSSL::PrivateKey(serverKey));
+  server.on("/", handle_OnConnect);
+  server.begin();
+  
+  Serial.println("ESP8266 server started.");
+  
+}
+
 void loop() {
+  serverHTTP.handleClient();
   server.handleClient();
-//  server.send(200, "text/html", sendHtml);
+  MDNS.update();
 }
---
+++ esp32Camera/espCamera/espCamera.ino
@@ -0,0 +1,148 @@
+#include "esp_camera.h"
+#include "SD_MMC.h"
+#include "FS.h"
+#include<EEPROM.h>
+#include <WiFi.h>
+#define EEPROM_SIZE 1
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+
+#include "camera_pins.h"
+//
+//const char* ssid = "U+Net505F";
+//const char* password = "wjswlsgur123!";
+
+const char* ssid = "JJH";
+const char* password = "Jeonjinhyeok0204";
+
+int pictureNumber = 0;
+
+void startCameraServer();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA);
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void takePicture(){ // 사진 찍는 함수
+   Serial.println("Try to take Picture");
+   camera_fb_t * fb = NULL;
+   fb = esp_camera_fb_get(); // 이게 사진 찍음
+   if(!fb){
+      Serial.println("Camera capture failed");
+      return;
+   }
+   // initialize EEPROM with predefined size
+   EEPROM.begin(EEPROM_SIZE);
+   pictureNumber = EEPROM.read(0)+1;
+
+   String path = "/picture" + String(pictureNumber) + ".jpg";
+    fs::FS &fs = SD_MMC;
+    Serial.printf("Picture file name: %s\n", path.c_str());
+
+    File file = fs.open(path.c_str(), FILE_WRITE); // 여기서 문제 생김 왜 ??
+    if(!file){
+      Serial.println("Failed to open file in writing mode");
+    }
+    else{
+      file.write(fb->buf, fb->len); // payload (image), payload length
+      Serial.printf("Saved file to path: %s\n", path.c_str());
+      EEPROM.write(0, pictureNumber);
+      EEPROM.commit();
+    }
+    file.close();
+    esp_camera_fb_return(fb);
+}
+
+
+void loop() {
+  char c = Serial.read(); // 버튼 입력이 촬영일 경우로 바꿔야 함!
+  if(c == 'c'){takePicture();}
+  
+  delay(10000);
+}
